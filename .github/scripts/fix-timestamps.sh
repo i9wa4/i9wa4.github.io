4175ab0fc43e457c9a4f8a56af46eb544d4487c0
i9wa4 <127664533+i9wa4@users.noreply.github.com>
2024-08-26 10:09:45 +0900

    modified:   .github/scripts/fix-timestamps.sh

--- .github/scripts/fix-timestamps.sh
+++ .github/scripts/fix-timestamps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# https://github.com/adityam/stochastic-control/blob/quarto/.github/scripts/fix-timestamps
 git ls-tree -r --name-only HEAD | while read filename; do
   unixtime=$(git log -1 --format="%at" -- "${filename}")
   touchtime=$(date -d @$unixtime +'%Y%m%d%H%M.%S')
